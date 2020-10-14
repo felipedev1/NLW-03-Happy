00aa0c8197f45555e2c36f8d77c14127b58f9eb0
Felipe <felipe.pereira.dev@gmail.com>
2020-10-14 19:46:35 -0300

    backend - orphanage model

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,13 @@
 import express from 'express'
 
 import './database/connection'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(routes)
 
-app.get('/', (req, res) => {
-  console.log('teste')
-  res.send('teste')
-})
+
 
 app.listen(3333)
