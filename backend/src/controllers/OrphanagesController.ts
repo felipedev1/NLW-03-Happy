e5ec62d8244c7441dc24ff9c9c6611d80ee439e7
Felipe <felipe.pereira.dev@gmail.com>
2020-10-14 22:10:12 -0300

    backend - image uploads

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -32,9 +32,15 @@ export default {
       opening_hours,
       open_on_weekends
     } = req.body
+
+    const requestImages = req.files as Express.Multer.File[]
+    
+    const images = requestImages.map(image => {
+      return { path: image.filename };
+    })
   
     const orphanagesRepository = getRepository(Orphanage)
-  
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -42,7 +48,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
     })
   
     await orphanagesRepository.save(orphanage)
