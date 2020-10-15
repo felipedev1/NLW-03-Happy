db532305fb489c5d198f5ba2a0704b306d51965d
Felipe <felipe.pereira.dev@gmail.com>
2020-10-15 19:23:01 -0300

    web - create orphanage page content and styles

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -54,7 +54,7 @@ function OrphanagesMap() {
           </Marker>
         </Map>
 
-        <Link to="" className="create-orphanage">
+        <Link to="/orphanages/create" className="create-orphanage">
           <FiPlus size={32} color="#FFF" />
         </Link>
       </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing'
 import OrphanagesMap from './pages/OrphanagesMap'
+import CreateOrphanage from './pages/CreateOrphanage'
 
 function Routes() {
   return (
@@ -10,6 +11,7 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/app" component={OrphanagesMap} />
+        <Route exact path="/orphanages/create" component={CreateOrphanage} />
       </Switch>
     </BrowserRouter>
   )
